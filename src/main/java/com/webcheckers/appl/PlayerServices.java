df76b58157cb31f6d51a29837ba5cd6990977a3d
Hi-Im-Kyle <kyleheberger@gmail.com>
2019-06-20 15:42:26 -0400

    Created a new folder called appl and moved the GameCenter and PlayerServices classes to it in order to better seperate the files.

---
+++ src/main/java/com/webcheckers/appl/PlayerServices.java
@@ -0,0 +1,32 @@
+package com.webcheckers.appl;
+
+/**
+ * The object to coordinate the state of the Web Application for a Player.
+ *
+ */
+
+public class PlayerServices {
+
+    // this Player's current game, if any.
+    //private CheckersGame game = null;
+
+    //this Players's GameCenter.
+    private final GameCenter gameCenter;
+
+    // this Player's unique username.
+    private String username;
+
+    /**
+     * Create a new {@linkplain PlayerServices} Player.
+     *
+     * Current game is set to null.
+     * @param username unique username
+     * @param gameCenter {@link GameCenter} for site wide responsibilities.
+     */
+    PlayerServices (String username, GameCenter gameCenter) {
+        this.username = username;
+        this.gameCenter = gameCenter;
+    }
+
+
+}
