c58acf8b8273348722c2e29c5bb9f20a8b175d9f
Hi-Im-Kyle <kyleheberger@gmail.com>
2019-06-21 16:33:31 -0400

    Added a nameIsTaken method that shows if the given username is already in the players list.

--- src/main/java/com/webcheckers/appl/GameCenter.java
+++ src/main/java/com/webcheckers/appl/GameCenter.java
@@ -12,7 +12,27 @@ public class GameCenter {
 
     private ArrayList<PlayerServices> players = new ArrayList<>();
 
+	/**
+     * Add the given PlayerServices to the players list.
+     *
+     * @param ps PlayerServices to add
+     */
     public void addPlayer(PlayerServices ps) {
         this.players.add(ps);
     }
+	
+	/**
+     * Check if a provided username is taken.
+     *
+     * @param name String to test
+     * @return true if username is taken, false otherwise.
+     */
+	public boolean nameIsTaken(String name) {
+		for (PlayerServices player : players) {
+			if (name.equals(player.Id())) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
