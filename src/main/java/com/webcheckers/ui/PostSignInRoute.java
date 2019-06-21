c162b8b92c407cff362a81080da454fac1c391e3
Hi-Im-Kyle <kyleheberger@gmail.com>
2019-06-21 16:39:13 -0400

    Made sure the new PlayerServices that is created after a login is added to the GameCenter's player list.

--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -109,6 +109,8 @@ public class PostSignInRoute implements Route {
 			  // create new player and add to session
 			  PlayerServices ps = new PlayerServices(username, this.gameCenter);
 			  request.session().attribute("PlayerServices", ps);
+			  // add the new player to the GameCenter's player list
+			  gameCenter.addPlayer(ps);
 			  //redirect back to home page
 			  response.redirect(WebServer.HOME_URL);
 			  return templateEngine.render(new ModelAndView(vm, "home.ftl"));
