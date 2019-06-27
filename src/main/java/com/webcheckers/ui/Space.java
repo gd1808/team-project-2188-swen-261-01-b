431e974b104d06622ae2bcf2dacef14a4c5157e5
Andrew Lee <axl8175@rit.edu>
2019-06-27 13:24:59 -0400

    Changed isValid to check if there is a Piece on the Space

--- src/main/java/com/webcheckers/ui/Space.java
+++ src/main/java/com/webcheckers/ui/Space.java
@@ -5,19 +5,24 @@ import com.webcheckers.ui.Piece;
 public class Space {
     private int cellIdx;
     private Piece piece;
-    private boolean isValid;
+    private boolean valid;
 
     public Space(int id, boolean valid) {
         this.cellIdx = id;
         this.piece = null;
-        this.isValid = valid;
+        this.valid = valid;
     }
 
     public int getCellIdx(){
         return this.cellIdx;
     }
     public boolean isValid(){
-        return this.isValid;
+        if(getPiece() != null) {
+            this.valid = true;
+        }else{
+            this.valid = false;
+        }
+        return this.valid;
     }
     public Piece getPiece(){
         return this.piece;
