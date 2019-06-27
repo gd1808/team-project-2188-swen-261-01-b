7578b32a017ce5316223f9a0b96c9eeb24d92ee9
U-SE\keh9654 <keh9654@FLAMESKULL.se.rit.edu>
2019-06-27 13:46:57 -0400

    Removed redundant import statement.

--- src/main/java/com/webcheckers/ui/Row.java
+++ src/main/java/com/webcheckers/ui/Row.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import com.webcheckers.ui.Space;
-
 public class Row implements Iterable<Space> {
 	private int index;
 	private List<Space> spaces;
