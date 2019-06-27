406b70cb836ce36536f4faf6ce4351e2645d20ba
Gabriel Diaz <gd1808@rit.edu>
2019-06-27 14:03:49 -0400

    Fixed some minor stuff and made rows a List and updated an if statement in Space...merge conflicts have been fixed

--- src/main/java/com/webcheckers/ui/BoardView.java
+++ src/main/java/com/webcheckers/ui/BoardView.java
@@ -2,13 +2,14 @@ package com.webcheckers.ui;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class BoardView implements Iterable<Row> {
-    private ArrayList<Row> rows;
+    private List<Row> rows;
 
     public BoardView() {
         rows = new ArrayList<>();
-        for (int i = 0; i < 7; ++i) {
+        for (int i = 0; i < 8; ++i) {
             rows.add(new Row(i));
         }
     }
@@ -18,3 +19,4 @@ public class BoardView implements Iterable<Row> {
         return rows.iterator();
     }
 }
+
--- src/main/java/com/webcheckers/ui/Row.java
+++ src/main/java/com/webcheckers/ui/Row.java
@@ -5,39 +5,39 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Row implements Iterable<Space> {
-	private int index;
-	private List<Space> spaces;
+    private int index;
+    private List<Space> spaces;
 
-	Row(int index) {
-		this.index = index;
-		spaces = new ArrayList<Space>();
-		for (int i = 0; i < 8; i++) {
-			Space space;
-			if (index % 2 == 0) {
-				if (i % 2 == 0) {
-					space = new Space(i, false);
-				}
-				else {
-					space = new Space(i, true);
-				}
-			}
-			else {
-				if (i % 2 == 0) {
-					space = new Space(i, true);
-				}
-				else {
-					space = new Space(i, false);
-				}
-			}
-			spaces.add(space);
-		}
-	}
+    Row(int index) {
+        this.index = index;
+        spaces = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Space space;
+            if (index % 2 == 0) {
+                if (i % 2 == 0) {
+                    space = new Space(i, false);
+                }
+                else {
+                    space = new Space(i, true);
+                }
+            }
+            else {
+                if (i % 2 == 0) {
+                    space = new Space(i, true);
+                }
+                else {
+                    space = new Space(i, false);
+                }
+            }
+            spaces.add(space);
+        }
+    }
 
-	public int getIndex() {
-		return index;
-	}
+    public int getIndex() {
+        return index;
+    }
 
-	public Iterator<Space> iterator() {
-		return spaces.iterator();
-	}
+    public Iterator<Space> iterator() {
+        return spaces.iterator();
+    }
 }
