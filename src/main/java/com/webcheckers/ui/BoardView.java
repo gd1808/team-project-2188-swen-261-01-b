0666f52cec5aff9a31c34127504f301771adeba3
Gabriel Diaz <gd1808@rit.edu>
2019-06-27 14:31:39 -0400

    Changed BoardView to take in the playerID as an int and added getter for playerID

--- src/main/java/com/webcheckers/ui/BoardView.java
+++ src/main/java/com/webcheckers/ui/BoardView.java
@@ -6,12 +6,18 @@ import java.util.List;
 
 public class BoardView implements Iterable<Row> {
     private List<Row> rows;
+    private int playerID;
 
-    public BoardView() {
+    public BoardView(int playerID) {
+        this.playerID = playerID;
         rows = new ArrayList<>();
         for (int i = 0; i < 8; ++i) {
             rows.add(new Row(i));
         }
+    }
+
+    public int getPlayerID() {
+        return playerID;
     }
 
     @Override
