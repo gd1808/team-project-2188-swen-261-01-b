deb4b89e1347f25bbf1ef8eb8afaf02017e34875
Gunther Kroth <gdkroth@gmail.com>
2019-06-28 07:05:57 -0400

    Create the Board class as the model-tier version of the Game.

---
+++ src/main/java/com/webcheckers/model/Board.java
@@ -0,0 +1,20 @@
+package com.webcheckers.model;
+
+import com.webcheckers.ui.Piece;
+
+/**
+ * A class to represent the checkers board for a Game.
+ */
+
+public class Board {
+
+    private Piece.Color activeColor;
+
+    public Board() {
+        this.activeColor = Piece.Color.RED;
+    }
+
+    public Piece.Color getActiveColor() {
+        return this.activeColor;
+    }
+}
