25a933daf8661470849f5de4f01784c6075ae5ed
Gunther Kroth <gdkroth@gmail.com>
2019-07-09 22:20:04 -0400

    Created an initialized state for the model-tier Board. Added a toString for it as well.

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -8,11 +8,44 @@ import com.webcheckers.ui.Piece;
 
 public class Board {
 
+    private Square[][] board;
+
     private Piece.Color activeColor;
 
     public Board() {
+        this.board = new Square[8][8];
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                // if row is even, white first
+                if (row % 2 == 0) {
+                    if (col % 2 == 0) {
+                        board[row][col] = new Square(Square.Color.WHITE);
+                    } else {
+                        board[row][col] = new Square(Square.Color.BLACK);
+                        if (row <= 2) {
+                            board[row][col].addPiece(Piece.Color.WHITE);
+                        } else if (row >= 5) {
+                            board[row][col].addPiece(Piece.Color.RED);
+                        }
+                    }
+                } else {
+                    if (col % 2 == 0) {
+                        board[row][col] = new Square(Square.Color.BLACK);
+                        if (row <= 2) {
+                            board[row][col].addPiece(Piece.Color.WHITE);
+                        } else if (row >= 5) {
+                            board[row][col].addPiece(Piece.Color.RED);
+                        }
+                    } else {
+                        board[row][col] = new Square(Square.Color.WHITE);
+                    }
+                }
+            }
+        }
         this.activeColor = Piece.Color.RED;
     }
+
+
 
     public Piece.Color getActiveColor() {
         return this.activeColor;
@@ -20,5 +53,30 @@ public class Board {
 
     public boolean isValidMove(Move move) {
         return true;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j].getColor() == Square.Color.BLACK) {
+                    if (board[i][j].getPiece() != null) {
+                        s += "[(B)]";
+                    } else {
+                        s += "[B]";
+                    }
+                } else {
+                    s += "[W]";
+                }
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        System.out.println(b);
     }
 }
