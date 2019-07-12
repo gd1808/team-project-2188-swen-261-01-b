3e377690f7fe227d6c885ccb398e76d74b6450bc
Hi-Im-Kyle <kyleheberger@gmail.com>
2019-07-11 23:04:56 -0400

    Updated the move field to be an array list of all validated moves, refined the is jump to work better with the rotated board, and added up to date move validation functionality. The only thing left to do with move validation is make more moves invalid if the piece is a king or single type.

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -2,6 +2,7 @@ package com.webcheckers.model;
 
 import com.webcheckers.ui.Piece;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -14,7 +15,7 @@ public class Board {
 
     private Piece.Color activeColor;
 
-    private Move currentMove;
+    private ArrayList<Move> moveList;
 	
 	private int whitePieces;
 	private int redPieces;
@@ -52,6 +53,7 @@ public class Board {
         this.activeColor = Piece.Color.RED;
 		this.whitePieces = 12;
 		this.redPieces = 12;
+		this.moveList = new ArrayList<>();
     }
 
 
@@ -64,13 +66,33 @@ public class Board {
         this.activeColor = activeColor;
     }
 
+	//This will flip the move if it is from white so that way it matches our board model's actual squares.
+	public Move getActualMove(Move move) {
+		//If the piece is red, don't flip it.
+		if (this.activeColor == Piece.Color.RED)
+			return move;
+		
+		Position start = move.getStart();
+        int startRow = 7 - start.getRow();
+        int startCol = 7 - start.getCell();
+		Position end = move.getEnd();
+        int endRow = 7 - end.getRow();
+        int endCol = 7 - end.getCell();
+		Position actualStart = new Position(startRow, startCol);
+		Position actualEnd = new Position(endRow, endCol);
+		Move actualMove = new Move(actualStart, actualEnd);
+		return actualMove;
+	}
+
     public boolean isValidMove(Move move) {
-        Position start = move.getStart();
+		Move actualMove = getActualMove(move);
+		
+        Position start = actualMove.getStart();
         int startRow = start.getRow();
         int startCol = start.getCell();
         Square startSquare = this.board[startRow][startCol];
 
-        Position end = move.getEnd();
+        Position end = actualMove.getEnd();
         int endRow = end.getRow();
         int endCol = end.getCell();
         Square endSquare = this.board[endRow][endCol];
@@ -79,9 +101,11 @@ public class Board {
         boolean validSquare = endSquare.isPlayable();
         // is the move diagonal
         boolean diagonalMove = isDiagonal(start, end);
+		// is the move a jump move
+		boolean jumpMove = isJump(start, end);
 
-        if (validSquare && diagonalMove) {
-            this.currentMove = move;
+        if (validSquare && (diagonalMove || jumpMove)) {
+            this.moveList.add(actualMove);
             return true;
         } else {
             return false;
@@ -89,20 +113,26 @@ public class Board {
 
     }
 
-    private boolean isJump(Position start){
-        int potjump = start.getCell() - 1;
-        int potjump1 = start.getCell() + 1;
-        int potrow = start.getRow() + 1;
-
-        if(board[potrow][potjump].getPiece() != null && board[potrow][potjump].getPiece().getColor() != activeColor){
-            return board[potrow + 1][potjump - 1].getPiece() == null;
-
-        }else if(board[potrow][potjump1].getPiece() != null && board[potrow][potjump].getPiece().getColor() != activeColor){
-            return board[potrow + 1][potjump + 1].getPiece() == null;
-
-        }else{
-            return false;
-        }
+    private boolean isJump(Position start, Position end){
+		int xMovement = start.getCell() - end.getCell();
+        int yMovement = start.getRow() - end.getRow();
+        if (Math.abs(xMovement) != 2) {
+			return false;
+		}
+		if (Math.abs(yMovement) != 2) {
+			return false;
+		}
+		System.out.println("startCell: " + start.getCell() + " startRow: " + start.getRow() + " endCell: " + end.getCell() + " endRow: " + end.getRow());
+		System.out.println("xMovement: " + xMovement + " yMovement: " + yMovement);
+		int jumpedOverCol = start.getCell() - (xMovement/2);
+		int jumpedOverRow = start.getRow() - (yMovement/2);
+		System.out.println("jumpedOverRow; " + jumpedOverRow + " jumpedOverCol; " + jumpedOverCol);
+		if (board[jumpedOverRow][jumpedOverCol].getPiece() != null && board[jumpedOverRow][jumpedOverCol].getPiece().getColor() != this.activeColor) {
+			int endCol = end.getCell();
+			int endRow = end.getRow();
+			return board[endRow][endCol].getPiece() == null;
+		}
+		return false;
     }
 
     private boolean isDiagonal(Position start, Position end) {
@@ -122,8 +152,13 @@ public class Board {
         // check for multi jumps and stuff
         boolean canBackUp = true; // change this flag if not possible
         if (canBackUp) {
-            this.currentMove = null;
-            return "true";
+			if (this.moveList.size() > 0) {
+				this.moveList.remove(this.moveList.size() - 1);
+				return "true";
+			}
+			else {
+				return "moveList is empty, cannot backup the move.";
+			}
         } else {
             String errorMessage = ""; // provide error message if not possible
             return errorMessage;
@@ -156,6 +191,40 @@ public class Board {
 	
 	public boolean teamIsEliminated() {
 		return whitePieces == 0 || redPieces == 0;
-
-    }
+	}
+	
+	//This should be what makes sure all the moves currently in move list are
+	//actually allowed in that order and this should also submit those changes
+	//to the game board. The string returned should be "true" if it was successful
+	//and what was wrong with the move if it was not successful
+	public String trySubmitTurn() {
+		
+		//Right now this defaults to true. Logic should be added to make sure that 
+		//all the moves in the move list are valid.
+		if (true) {
+			if (this.moveList.size() > 0) {
+				//Get the last move for now and just assign its ending position to the
+				//piece that jumped there.
+				Position pieceInitialPosition = this.moveList.get(0).getStart();
+				Position pieceEndingPosition = this.moveList.get(this.moveList.size() - 1).getEnd();
+				int initialRow = pieceInitialPosition.getRow();
+				int initialCol = pieceInitialPosition.getCell();
+				Piece initialPiece = this.board[initialRow][initialCol].getPiece();
+				int endRow = pieceEndingPosition.getRow();
+				int endCol = pieceEndingPosition.getCell();
+				this.board[endRow][endCol].addPiece(initialPiece.getColor());
+				if (initialPiece.getType() == Piece.Type.KING) {
+					this.board[endRow][endCol].getPiece().makeKing();
+				}
+				this.board[initialRow][initialCol].removePiece();
+			}
+			else {
+				return "No moves!";
+			}
+			//The turn was submitted correctly!
+			moveList.clear();
+			return "true";
+		}
+		return "Invalid move!";
+	}
 }
