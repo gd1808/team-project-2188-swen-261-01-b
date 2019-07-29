0bec96a7381c8e0e7de2379dddbeb5d70c7c7a1a
Gunther Kroth <gdkroth@gmail.com>
2019-07-29 07:35:07 -0400

    Implemented GetReplayStopWatchingRoute used to exit a game that is being replayed.

--- src/main/java/com/webcheckers/ui/GetReplayStopWatchingRoute.java
+++ src/main/java/com/webcheckers/ui/GetReplayStopWatchingRoute.java
@@ -1,18 +1,46 @@
 package com.webcheckers.ui;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
-import spark.TemplateEngine;
+import com.webcheckers.appl.PlayerServices;
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 public class GetReplayStopWatchingRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetReplayStopWatchingRoute.class.getName());
 
+    // FreeMarker
+    private final TemplateEngine templateEngine;
+
+    /**
+     * Create the Spare Route (UI controller) to handle all {@code GET /replay/stopWatching} HTTP requests.
+     *
+     * @param templateEngine the HTML template rendering engine
+     */
     public GetReplayStopWatchingRoute(TemplateEngine templateEngine) {
-
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        LOG.config("etReplayStopWatchingRoute is initialized.");
     }
 
+    /**
+     *
+     * @param request the HTTP request
+     * @param response the HTTP response
+     * @return the rendered HTML for the home page
+     */
     @Override
-    public Object handle(Request request, Response response) throws Exception {
-        return null;
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetReplayStopWatchingRoute is invoked.");
+
+        PlayerServices current = request.session().attribute("PlayerServices");
+        current.endCurrentGame();
+
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Welcome!");
+
+        response.redirect(WebServer.HOME_URL);
+        return templateEngine.render(new ModelAndView(vm, "home.ftl"));
     }
 }
